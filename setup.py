4cfadb0604c36552394685930714c36fbc2c715c
Bartosz Hanc <bartosz.hanc02@gmail.com>
2025-05-12 13:25:10 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -4,4 +4,11 @@ setup(
     name="tinynn",
     version="0.1",
     packages=find_packages(),
+    license="MIT",
+    description="Tiny neural network library written from scratch using only Numpy.",
+    install_requires=[
+        "numpy",
+        "matplotlib",
+    ],
+    python_requires=">=3.10",
 )
